d0886f10b15095a0a8a634d068852be4c34c82b5
chris.centore@gmail.com <chris.centore@gmail.com@d91d4f3c-f9b2-602f-a116-d76e34294a7d>
2011-12-27 19:53:13 +0000

    new options in config, new sqlite output option as well.

--- src/output.py
+++ src/output.py
@@ -31,16 +31,35 @@ class Output(object):
 
                 	outputfile.write(self.modname + ": " + self.msg + "\n")
                 	outputfile.close()
-
-                
+		def to_Sqlite3():
+			import sqlite3
+			path = check_config("OUTPUT_SQLITE3_DB_PATH=")
+			if not os.path.isfile(path):
+				conn = sqlite3.connect(path)
+				c = conn.cursor()
+				c.execute('create table spicymango (modname text, msg text)')
+			else:
+				conn = sqlite3.connect(path)
+				c = conn.cursor()
+			c.execute("insert into spicymango values ('" + self.modname + "','" + self.msg + "')")
+			conn.commit()
+			c.close()
+			                
 		#Check config and send output where enabled, but first make sure required attributes are set.
 		if self.modname is None:
 			print_error("Output", "modname attribute must be defined in instance")
 		
+		#Counter var to determine if any ouput is enabled.
+		output_count = 0
+		
 		file_enable = check_config("OUTPUT_FILE")
         	if file_enable == "ON":
 			to_File()
-		
+			output_count = 1
+		sqlite3_enable = check_config("OUTPUT_SQLITE3=")
+		if sqlite3_enable == "ON":
+			to_Sqlite3()
+			output_count = 1
 		#If no output destinations are defined in config, send output to console
-		else:
-			print self.modname + ": " + self.msg
+		if output_count == 0 or check_config("OUTPUT_CONSOLE=") == "ON":
+			print '\033[94m' + self.modname + ": " + '\033[0m' + self.msg
