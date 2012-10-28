931463ba7e4aff10dbe0c18bae7e3aa8f905500d
antitree@gmail.com <antitree@gmail.com@d91d4f3c-f9b2-602f-a116-d76e34294a7d>
2012-10-28 14:00:59 +0000

    Modified to support Tor libraries

--- modules/mod_rss.py
+++ modules/mod_rss.py
@@ -1,7 +1,7 @@
 #########################################################################
 # Filename: mod_rss.spy
 # Description: This module reads RSS feeds from a text file
-# Copyright (C) 2011-2012 AntitreE
+# Copyright (C) 2011-2012 Antitree
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -28,6 +28,7 @@ from src.core import *
 from src.output import *
 import urllib2, urllib, time
 from includes import feedparser
+from includes import TorSocks
 import re
 
 #Read in code to dynamically get the module name.
@@ -36,9 +37,16 @@ execfile('src/getname')
 #Get configuration options
 interval = int(check_config("MOD_RSS_INTERVAL="))
 
-def main(query,keywords,*args):
+def torup():
+	global torhost, torport, torctlport, torpass
+	torhost = check_config("TOR_HOST=")
+        torport = int(check_config("TOR_PORT="))
+        torctlport = int(check_config("TOR_CTL_PORT="))
+        torpass = check_config("TOR_PASS=")
+
+def main(query,keywords,tor,*args):
 	global interval
-	
+
 	while True:
 		author = ''
 		desc = ''
@@ -47,13 +55,35 @@ def main(query,keywords,*args):
 
 
 		url = query
+		if tor == "ON":
+		    try:
+			t = TorSocks.Tor(torhost, torctlport, torpass)
+			if t.get_status():
+				print_status(module, "Successfully connected to Tor")
+			t.get_new_circuit()
+			torconn = TorSocks.URLOpener(torport,torhost)
+
+		  	feed = torconn.get_url(url)
+			##TODO modify scraped results to remove HTTP requests of
+			##  anonymized content
+			#re.sub("http://", 'PROTECTED://',feed)
+			#print(torconn.get_url("http://www.ifconfig.me/ip")).read()
+		    except Exception as e:
+			print_error(module, "Error connecting to Tor. " +
+				"make sure the control port is accessible, " + 
+				"the SOCKS port is correct in your config file " +
+				"and Tor is actually started. %s" % e)
+			break
+		else:
+			feed = url
+		rss = feedparser.parse(feed)
+		print_status(module, 'Collecting feed %s...%s' % (url[0:15],url[-5:]))
+
+		#Enable this print to see the raw data dump for troubleshooting
+		#print rss
+
+
 		try:
-		  rss = feedparser.parse(url)
-		  print_status(module, 'Collecting feed %s...%s' % (url[0:15],url[-5:]))
-
-		  #Enable this print to see the raw data dump for troubleshooting
-		  #print rss
-
 		  for e in rss.entries:
 			if "title" in e:
 				title = e.title
@@ -85,9 +115,10 @@ def main(query,keywords,*args):
 				modOutput.send_output()
 			   except KeyError:
 				#pass
-				print("error!!")
+				print("Output error!!")
 	    	except:
 		  print_error(module, "Could not parse url: %s  check to make sure it is a valid RSS or Atom feed" % url)
+		  break
 		#Set delay should be at least an hour for RSS feeds
 		time.sleep(interval)
 
@@ -97,7 +128,11 @@ feeds = get_feeds("mod_rss")
 if len(feeds) > 0:
         print_status(module,"loading...")
         for feed in feeds:
-                thread.start_new_thread(main, (feed[0],feed[1],2))
+		if feed[2] == "ON":
+			torup()
+			TorSocks.Tor(torhost, torctlport, torpass).get_new_circuit()
+			time.sleep(1) ##workaround to prevent the same circuits from being used
+                thread.start_new_thread(main, (feed[0],feed[1],feed[2],2))
 
 else:
         print_error(module, "No feeds defined for this module")
